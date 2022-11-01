41fb3fd0353fd562e0ec6fbacc1f5287e70376f1
Georgi Georgiev <auratiger00@gmail.com>
2022-11-01 20:43:08 +0200

    feat: clean up and setup absolute paths

---
+++ gatsby-node.ts
@@ -0,0 +1,19 @@
+const path = require("path");
+
+// Sets up Webpack import aliases. If using typescript,
+// the corresponding location values should be specified in the 'paths' property of the tsconfig.json file
+exports.onCreateWebpackConfig = ({ actions }) => {
+   actions.setWebpackConfig({
+      resolve: {
+         alias: {
+            "@src": path.resolve(__dirname, "src"),
+            "@homepage": path.resolve(__dirname, "src/modules/homepage"),
+            "@common": path.resolve(__dirname, "src/modules/common"),
+            "@constants": path.resolve(__dirname, "src/constants"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@assets": path.resolve(__dirname, "src/images/assets"),
+            "@mixins": path.resolve(__dirname, "src/mixins"),
+         },
+      },
+   });
+};
